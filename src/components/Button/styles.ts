3cb5083d9a6d6a76f3b3df3565f52e89af49a608
Gabriel Soares <gabrsoares@outlook.com>
2022-12-16 18:14:15 -0300

    projeto

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #1C1C1C;
+    color: #FFF;
+
+    border: 1px solid #1C1C1C;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+    
+`
